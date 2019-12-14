823e20562f05f62cc71ec70b7f2cc9a3e2e0db75
Jannis R <mail@jannisr.de>
2019-12-14 11:17:30 +0100

    fix arrival/departure timestamp :bug:, minor changes, 1.0.1

--- arrival-departure.js
+++ arrival-departure.js
@@ -8,27 +8,27 @@ const matrix = (n, m) => m.reduce((l, m) => {
 }, [])
 
 const arrivalDepartureIds = (stopIds, tripIds, routeIds, lineIds, normalizePlatform) => (type, _) => {
-	const when = new Date(_.plannedWhen) / 1000 | 0
+	const when = Math.round(new Date(_.plannedWhen) / 1000)
 	const platform = _.plannedPlatform ? normalizePlatform(_.plannedPlatform) : null
 
-	// This assumes that no two vehicles
-	// - running for the same route
-	// - stop at the same station
+	// This assumes that there are no two vehicles
+	// - running for the same route,
+	// - stop at the same station,
 	// - at the same time.
 	const fuzzyByRoute = !Number.isNaN(when)
 		? matrix(stopIds, routeIds).map(id => [...id, when])
 		: []
 
-	// This assumes that no two vehicles
-	// - running for the same line
-	// - stop at the same station
-	// - at the same platform
+	// This assumes that there are no two vehicles
+	// - running for the same line,
+	// - stop at the same station,
+	// - at the same platform,
 	// - at the same time.
 	const fuzzyByLine = !Number.isNaN(when) && platform
 		? matrix(stopIds, lineIds).map(id => [...id, when, platform])
 		: []
 	return [
-		...matrix(stopIds, tripIds),
+		...matrix(stopIds, tripIds), // todo: arr time
 		...fuzzyByRoute,
 		...fuzzyByLine
 	].map(id => [v, type, ...id].join(':'))
